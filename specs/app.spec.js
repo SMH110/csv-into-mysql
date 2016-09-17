40417e1cef3d42c7515c6cae4a82a36b64209e78
SMH <mahdihassan135@hotmail.com>
2016-09-17 15:15:55 +0100

    Write test for --fiiles flag

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -5,33 +5,111 @@ const fs = require('fs'),
     DB_CONFIG = require('../dbconfig.json'),
     sinon = require('sinon');
 
-var exec = exec;
+describe('When the user has csv files in the files folder', function () {
+    this.timeout(10000);
 
+    beforeEach(function () {
+        return ensureTablesRemoved(['t1', 't2', 'tr']);
+    });
 
+    afterEach(function () {
+        return ensureTablesRemoved(['t1', 't2', 'tr']);
+    });
 
-// describe('When the user puts csv files in the files folder', function () {
-//     this.timeout(5000);
+    describe('And runs the app.js from the command line with no options', function () {
+        beforeEach(() => {
+            return runCommand('node app.js');
+        });
 
-//     describe('And runs the app.js from the command line', function () {
-//         beforeEach(function () {
-//             deleteTableIfExist();
-//             return runProcess('node app.js');
-//         });
+        it('Should complete without error', function () {
+        });
 
+        it('Should have inserted the t1 csv as expected', function (done) {
+            var connection = mysql.createConnection(DB_CONFIG);
+            connection.query('SELECT * FROM t1', (error, data) => {
+                connection.end();
+                if (error) {
+                    done(error);
+                }
+                expect(data).to.deep.equal([
+                    { '"id"': "1", 'first name': '"SMH"', "person's email": 'smh@email.com' },
+                    { '"id"': "2", 'first name': "MO'", "person's email": 'mo@email.com' },
+                    { '"id"': "3", 'first name': 'hello, world', "person's email": 'someemail@email.com' }
+                ]);
+                done();
+            });
+        });
 
-//         afterEach(function () {
-//             deleteTableIfExist();
-//         });
+        it('Should have inserted the t2 csv as expected', function (done) {
+            var connection = mysql.createConnection(DB_CONFIG);
+            connection.query('SELECT * FROM t2', (error, data) => {
+                connection.end();
+                if (error) {
+                    done(error);
+                }
+                expect(data).to.deep.equal([
+                    { 'COL1': '"Hello, World!"', 'COL2': '```' }
+                ]);
+                done();
+            });
+        });
 
+        it('should end the connection after finishing inserting the csv files', function () {
 
-//         it('should do what...', function () {
-//             expect("SMH").to.be.equal("SMH");
-//         });
-//     });
-// });
- deleteTableIfExist();
+        });
+    });
 
-function runProcess(command) {
+    describe('When the user runs app.js using the --files flag', function () {
+
+        beforeEach(function () {
+            return runCommand('node app.js --files tr.csv');
+        });
+
+        it('Should have inserted the tr csv as expected', function (done) {
+            var connection = mysql.createConnection(DB_CONFIG);
+            connection.query('SELECT * FROM tr', (error, data) => {
+                connection.end();
+                if (error) {
+                    done(error);
+                }
+                expect(data).to.deep.equal([
+                    { '"id"': '1', 'first name': '"SMH"', 'person\'s email': 'smh@email.com' },
+                    { '"id"': '2', 'first name': 'MO\'', 'person\'s email': 'mo@email.com' },
+                    { '"id"': '3', 'first name': 'hello, world', 'person\'s email': 'someemail@email.com' },
+                    { '"id"': '4', 'first name': 'john', 'person\'s email': 'john@email.com' }
+                ]);
+                done();
+            });
+        });
+
+        it('Should have not inserted t1 csv in the files folder', function (done) {
+            var connection = mysql.createConnection(DB_CONFIG);
+            connection.query('SHOW TABLES LIKE \'t1\'', (error, data) => {
+                connection.end();
+                if (error) {
+                    done(error);
+                }
+                expect(data.length).to.deep.equal(0);
+                done();
+            });
+        });
+
+        it('Should have not inserted t2 csv in the files folder', function (done) {
+            var connection = mysql.createConnection(DB_CONFIG);
+            connection.query('SHOW TABLES LIKE \'t2\'', (error, data) => {
+                connection.end();
+                if (error) {
+                    done(error);
+                }
+                expect(data.length).to.deep.equal(0);
+                done();
+            });
+        });
+
+    });
+});
+
+function runCommand(command) {
     return new Promise((resolve, reject) => {
         require('child_process').exec(command, (error, stdout, stderr) => {
             if (error) {
@@ -48,40 +126,24 @@ function runProcess(command) {
     });
 }
 
-
-function deleteTableIfExist() {
-
-    console.log("deleteTableIfExist have been run :)");
-    fs.readdir('../files', 'utf8', (error, files) => {
-        if (error) {
-            console.error(error);
-            return;
-        }
+function ensureTablesRemoved(tables) {
+    return new Promise((resolve, reject) => {
         let connection = mysql.createConnection(DB_CONFIG);
-        files.forEach((file, index) => {
-            let tableName = convertFileNameToTableName(file);
 
-            connection.query(`DROP TABLE IF EXISTS ${tableName}`, (error, result) => {
+        var numberOfTablesProcessed = 0;
+        tables.forEach(table => {
+            connection.query(`DROP TABLE IF EXISTS ${table}`, error => {
+                numberOfTablesProcessed++;
+
                 if (error) {
-                    console.error(error);
-                    return;
+                    reject(error);
                 }
 
-                console.log(result);
-                if (files.length === ++index) {
+                if (numberOfTablesProcessed === tables.length) {
                     connection.end();
+                    resolve();
                 }
             });
-
         });
     });
 }
-
-
-
-function convertFileNameToTableName(file) {
-    file = file.slice(0, -4).replace(/`/g, "``");
-    return "`" + file + "`";
-}
-
-
